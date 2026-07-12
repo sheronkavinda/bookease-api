7d0441f0774b11c16fa018f8780942e4c0106e5b
sheronkavinda <it23603790@my.sliit.lk>
2026-07-12 14:04:39 +0530

    chore: create core application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ServicesModule } from './services/services.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
   imports: [
@@ -25,6 +29,14 @@ import { AppService } from './app.service';
         synchronize: true,
       }),
     }),
+
+    UsersModule,
+
+    AuthModule,
+
+    ServicesModule,
+
+    BookingsModule,
   ],
 
   controllers: [AppController],
