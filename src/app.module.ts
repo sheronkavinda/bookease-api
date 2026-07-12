1590dc4f23152619317737f73352732433e1a5e7
sheronkavinda <it23603790@my.sliit.lk>
2026-07-12 13:42:46 +0530

    chore: configure PostgreSQL and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
